5a4cdee396b43fccd354a7da167d15bf16b4988a
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-10-21 16:12:43 -0700

    working routes and some css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,10 +4,9 @@ const withAuth = require('../utils/auth');
 // god get router
 router.get('/', async (req, res) => {
     console.log('first');
-    let postData;
     try {
         console.log('second');
-        postData = await Posts.findAll({
+      const postData = await Posts.findAll({
             include: [
                 {
                     model: User,
@@ -17,12 +16,14 @@ router.get('/', async (req, res) => {
         });
         console.log('adrian');
 
-        const allPost = postData.map((post) => post.get({ plain: true }));
+        const posts = postData.map((post) => post.get({ plain: true }));
 
         res.render('homepage', {
-            allPost,
-            logged_in: req.session.logged_in,
+
+            posts,
+            // logged_in: req.session.logged_in,
         });
+        console.log('homepage render');
     } catch (err) {
         res.status(500).json({ message: 'not working' });
     }
@@ -74,6 +75,31 @@ router.get('/profile', withAuth, async (req, res) => {
         res.status(505).json({ message: 'Couldnt find user based on the session ID.' })
     }
 });
+
+// router.get('/homepage', withAuth, async (req, res) => {
+//     try {
+//         const userData = await User.findByPk(req.session.user_id, {
+//             attributes: { exclude: ['password'] },
+//             include: [{ model: Posts }],
+//         });
+//         const user = userData.get({ plain: true });
+
+//         res.render('homepage', {
+//             ...user,
+//             logged_in: false
+//         });
+//     } catch (err) {
+//         res.status(505).json({ message: 'Couldnt find user based on the session ID.' })
+//     }
+// });
+
+// render.get('/dashboard', async (req, res) => {
+//     try {
+//         const userData = await User.findAll({
+
+//         })
+//     }
+// })
 
 //TODO get login redirect to dashboard
 router.get('/login', (req, res) => {
