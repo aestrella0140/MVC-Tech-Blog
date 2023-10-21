280b44135a3a947f23f1135f777aad1a0eae4046
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-10-21 16:18:38 -0700

    trying to debug heroku

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
         res.render('homepage', {
 
             posts,
-            // logged_in: req.session.logged_in,
+            logged_in: req.session.logged_in,
         });
         console.log('homepage render');
     } catch (err) {
